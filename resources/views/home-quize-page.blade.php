e4d0446e786aa4c4ae4c2b3ce0fc7d324fc9a0aa
brijesh-echttech <brijesh.sharma@echttech.com>
2024-12-24 13:22:55 +0530

    image add

--- resources/views/home-quize-page.blade.php
+++ resources/views/home-quize-page.blade.php
@@ -17,8 +17,7 @@
 
     <!-- Slick JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 </head>
 
 <body>
