e379ee94cd7106eccc9609b5286c72fd51ed3dee
Echt Tech <rahul.chaurasia@echttech.com>
2024-12-24 13:34:37 +0530

    front-page

--- resources/views/quiz/home.blade.php
+++ resources/views/quiz/home.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../resources/css/style.css">
+    <link rel="stylesheet" href="{{asset('assets/css/custom-style.css')}}">
     <title>Allahabad Kumbh</title>
     <!-- jQuery CDN -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
